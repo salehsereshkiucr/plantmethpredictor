36568325d865f3b30733057ad752941217ee4165
Saleh Sereshki <ssere004@ucr.edu>
2021-09-01 17:33:42 -0700

    fixed for SEQOnly, again

--- main.py
+++ main.py
@@ -63,6 +63,6 @@ for cnfg in cnfgs:
                 mode = 'Seq' + at
                 res.append(run_experiment(organism_name, context, mode, root, i))
                 np.savetxt("GFG.csv", res, delimiter =", ", fmt ='% s')
-            mode = 'SeqOnly'
-            res.append(run_experiment(organism_name, context, mode, root, i))
+            mode = anno_types[0]
+            res.append(run_experiment(organism_name, context, mode, root, i, is_seqonly=True))
             np.savetxt("GFG.csv", res, delimiter =", ", fmt ='% s')
