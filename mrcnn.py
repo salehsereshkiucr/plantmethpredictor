cca64fae063a35ad41ea63645788cff518ea55ce
Saleh Sereshki <ssere004@ucr.edu>
2021-10-05 09:56:58 -0700

    cpgeniemine completed

--- mrcnn.py
+++ mrcnn.py
@@ -6,8 +6,8 @@ from sklearn.model_selection import train_test_split
 import configs as configs
 
 
-num_steps = 200
-batch_size = 128
+num_steps = 1
+batch_size = 32
 display_step = 10
 
 
@@ -25,10 +25,8 @@ def load_data(root, organism_name, context, i, mode):
     Y = b
     X = X.reshape(list(X.shape) + [1])
     X = np.swapaxes(X, 1, 2)
-
     PROFILE_COLS = X.shape[1]
     PROFILE_ROWS = X.shape[2]
-
     test_percent = 0.2
     test_val_percent = 0.5
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=test_percent, random_state=None)
@@ -36,16 +34,16 @@ def load_data(root, organism_name, context, i, mode):
     return x_train, x_test, y_train, y_test
 
 def weight_variable(shape):
- initial = tf.truncated_normal(shape, stddev=0.1)
- return tf.Variable(initial)
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
 
 
 
 
 
 def bias_variable(shape):
- initial = tf.constant(0.1, shape=shape)
- return tf.Variable(initial)
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
 
 def net_MRCNN(x_fs):
@@ -58,28 +56,24 @@ def net_MRCNN(x_fs):
     W_conv2 = weight_variable([3, 3, 16, 32])
     b_conv2 = bias_variable([32])
     h_conv2 = tf.nn.relu(tf.nn.conv2d(h_conv1, W_conv2, strides=[1,1,1,1], padding='VALID') + b_conv2)
-    h_pool2 = tf.nn.max_pool(h_conv2,ksize=[1,3,3,1], strides=[1,3,3,1], padding='VALID')
+    h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1,3,3,1], strides=[1,3,3,1], padding='VALID')
     print (h_pool2)
-
     W_conv3 = weight_variable([3, 3, 32, 48])
     b_conv3 = bias_variable([48])
     h_conv3 = tf.nn.conv2d(h_pool2, W_conv3, strides=[1,1,1,1], padding='VALID')+ b_conv3
     W_conv4 = weight_variable([3, 3, 48, 64])
     b_conv4 = bias_variable([64])
     h_conv4 = tf.nn.conv2d(h_conv3, W_conv4, strides=[1,1,1,1], padding='VALID')+ b_conv4
-
     W_fc1 = weight_variable([2*2*64, 80])
     b_fc1 = bias_variable([80])
     h_pool4 = tf.reshape(h_conv4, [-1, 2*2*64])
     h_fc1 = tf.matmul(h_pool4, W_fc1) + b_fc1
     h_fc1_drop = tf.nn.dropout(h_fc1, 0.5)
     print (h_fc1_drop)
-
     W_fc2  = weight_variable([80, 1])
     b_fc2  = bias_variable([1])
     y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
     print (y_conv)
-
     return y_conv
 
 tf.disable_eager_execution()
@@ -93,33 +87,28 @@ for at in configs.Arabidopsis_config['annot_types']:
     mode+=at
 
 x_train, x_test, y_train, y_test = load_data(root, organism_name, context, i, mode)
-X = tf.placeholder(tf.float32, [None, len(x_train.shape)])
-Y = tf.placeholder(tf.float32, [None, len(y_train.shape)])
+X = tf.placeholder(tf.float32, [None]+list(x_train.shape)[1:])
+Y = tf.placeholder(tf.float32, [None]+list(y_train.shape)[1:])
 keep_prob = tf.placeholder(tf.float32)
 
 logits = net_MRCNN(X)
 prediction = tf.nn.softmax(logits)
 loss_op = tf.reduce_mean(tf.square(Y - logits), reduction_indices=[1])
-optimizer = tf.train.AdamOptimizer(1e-3).minimize(loss_op)
+optimizer = tf.train.AdamOptimizer(1e-3)
 train_op = optimizer.minimize(loss_op)
 
 correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 init = tf.global_variables_initializer()
-
+tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
 with tf.Session() as sess:
-
-    # Run the initializer
     sess.run(init)
-
     for step in range(1, num_steps+1):
         for bs_num in range(int(len(x_train) / batch_size) - 1):
             batch_x, batch_y = x_train[bs_num*batch_size: (bs_num+1)*batch_size], y_train[bs_num*batch_size: (bs_num+1)*batch_size]
             # Run optimization op (backprop)
             sess.run(train_op, feed_dict={X: batch_x, Y: batch_y, keep_prob: 0.8})
-
-    # Calculate accuracy for 256 MNIST test images
     print("Testing Accuracy:", \
         sess.run(accuracy, feed_dict={X: x_test,
                                       Y: y_test,
--- mrcnn_mine.py
+++ mrcnn_mine.py
@@ -10,9 +10,13 @@ import configs as configs
 import pandas as pd
 
 
-def run_experiment(organism_name, context, i, root):
-    X = np.load(root + organism_name +'/profiles/' + str(i) + '/X_' + context + '__' + organism_name + '.npy', allow_pickle=True)
-    Y = np.load(root + organism_name +'/profiles/' + str(i) + '/Y_' + context + '__' + organism_name + '.npy', allow_pickle= True)
+def run_experiment(organism_name, context, i, root, mode):
+
+    X = np.load(root + organism_name +'/profiles/' + str(i) + '/X_' + context + '_' + mode +'_' + organism_name + '.npy', allow_pickle=True)
+    X = np.delete(X, range(4,X.shape[2]), 2)
+    b = [j for j in range(300)] + [j for j in range(700, 1000)]
+    X = np.delete(X, b, 1)
+    Y = np.load(root + organism_name +'/profiles/' + str(i) + '/Y_' + context + '_' + mode +'_' + organism_name + '.npy', allow_pickle= True)
     Y = np.asarray(pd.cut(Y, bins=2, labels=[0, 1], right=False))
     b = np.zeros((Y.size, Y.max()+1))
     b[np.arange(Y.size),Y] = 1
@@ -31,66 +35,23 @@ def run_experiment(organism_name, context, i, root):
     W_maxnorm = 3
 
     model = Sequential()
-
-
-    W_conv2 = weight_variable([3, 3, 16, 32])
-    b_conv2 = bias_variable([32])
-    h_conv2 = tf.nn.relu(tf.nn.conv2d(h_conv1, W_conv2, strides=[1,1,1,1], padding='VALID') + b_conv2)
-    h_pool2 = tf.nn.max_pool(h_conv2,ksize=[1,3,3,1], strides=[1,3,3,1], padding='VALID')
-    print (h_pool2)
-
-    W_conv3 = weight_variable([3, 3, 32, 48])
-    b_conv3 = bias_variable([48])
-    h_conv3 = tf.nn.conv2d(h_pool2, W_conv3, strides=[1,1,1,1], padding='VALID')+ b_conv3
-    W_conv4 = weight_variable([3, 3, 48, 64])
-    b_conv4 = bias_variable([64])
-    h_conv4 = tf.nn.conv2d(h_conv3, W_conv4, strides=[1,1,1,1], padding='VALID')+ b_conv4
-
-    W_fc1 = weight_variable([2*2*64, 80])
-    b_fc1 = bias_variable([80])
-    h_pool4 = tf.reshape(h_conv4, [-1, 2*2*64])
-    h_fc1 = tf.matmul(h_pool4, W_fc1) + b_fc1
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-    print (h_fc1_drop)
-
-    W_fc2  = weight_variable([80, 1])
-    b_fc2  = bias_variable([1])
-    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-    print (y_conv)
-    loss = tf.reduce_mean(tf.square(y - y_conv),reduction_indices=[1])
-    train_step = tf.train.AdamOptimizer(1e-3).minimize(loss)
-
-
-
-
-
-    model.add(Conv2D(16, kernel_size=(1, 4), input_shape=(PROFILE_COLS, PROFILE_ROWS, 1), padding='VALID', use_bias=True))
+    model.add(Conv2D(16, kernel_size=(4, 1), input_shape=(PROFILE_COLS, PROFILE_ROWS, 1), padding='VALID', use_bias=True))
     model.add(Reshape((20, 20, 16), input_shape=(PROFILE_ROWS, 1, 16))) #end of first
-
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu',  padding='VALID', use_bias=True))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(3,3)))
-
-    ksize=[1,3,3,1], strides=[1,3,3,1], padding='VALID'
-
-    W_conv2 = weight_variable([3, 3, 16, 32])
-    b_conv2 = bias_variable([32])
-    h_conv2 = tf.nn.relu(tf.nn.conv2d(h_conv1, W_conv2, strides=[1,1,1,1], padding='VALID') + b_conv2)
-    h_pool2 = tf.nn.max_pool(h_conv2,ksize=[1,3,3,1], strides=[1,3,3,1], padding='VALID')
-    print (h_pool2)
-
-    model.add(Conv2D(512, kernel_size=(1, 5), activation='relu', padding='same', kernel_constraint=max_norm(W_maxnorm)))
-    model.add(MaxPooling2D(pool_size=(1, 5), strides=(1,3)))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(3,3), padding='VALID')) #end of second
+    model.add(Conv2D(48, kernel_size=(3, 3), padding='VALID', use_bias=True))
+    model.add(Conv2D(64, kernel_size=(3, 3), padding='VALID', use_bias=True)) #end of third
+    model.add(Reshape((-1, 2*2*64), input_shape=(2, 2, 64)))
     model.add(Flatten())
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(80, activation='relu', use_bias=True))
     model.add(Dropout(0.5))
     model.add(Dense(2))
     model.add(Activation('softmax'))
 
-    myoptimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-06)
-    model.compile(loss='binary_crossentropy', optimizer=myoptimizer,metrics=['accuracy'])
-    model.fit(x_train, y_train, batch_size=100, epochs=20, verbose=0, validation_split=0.1)
+
+    myoptimizer = keras.optimizers.Adam(lr=0.001)
+    model.compile(loss='mse', optimizer=myoptimizer, metrics=['accuracy'])
+    model.fit(x_train, y_train, batch_size=128, epochs=20, verbose=0, validation_split=0.1)
 
     y_pred = model.predict(x_test)
     y_pred = np.argmax(y_pred, axis=1)
@@ -100,7 +61,7 @@ def run_experiment(organism_name, context, i, root):
     return step_res
 
 
-root = '/home/csgrads/ssere004/output_cpgenie/'
+root = '/home/csgrads/ssere004/output_cpgenieannot/'
 contexts = ['CG', 'CHG', 'CHH']
 res=[]
 
@@ -111,21 +72,16 @@ res=[]
 #    res.append(run_experiment(organism_name, context, i, root))
 #    np.savetxt("GFG_cpgenie.csv", res, delimiter=", ", fmt='% s')
 
-cnfgs = [configs.Cowpea_config, configs.Rice_config, configs.Cucumber_config, configs.Tomato_config]
+cnfgs = [configs.Arabidopsis_config, configs.Cowpea_config, configs.Rice_config, configs.Cucumber_config, configs.Tomato_config]
+cnfgs = [configs.Arabidopsis_config]
 for cnfg in cnfgs:
     organism_name = cnfg['organism_name']
     for i in range(1,2):
         for context in contexts:
-            res.append(run_experiment(organism_name, context, i, root))
+            mode = 'Seq'
+            for at in configs.Arabidopsis_config['annot_types']:
+                mode+=at
+            res.append(run_experiment(organism_name, context, i, root, mode))
             np.savetxt("GFG_cpgenie.csv", res, delimiter=", ", fmt='% s')
 
 
-
-def weight_variable(shape):
- initial = tf.compat.v1.truncated_normal(shape, stddev=0.1)
- return tf.compat.v1.Variable(initial)
-
-
-def bias_variable(shape):
- initial = tf.compat.v1.constant(0.1, shape=shape)
- return tf.compat.v1.Variable(initial)
