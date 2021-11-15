d721fbb38b9e61d513895bd385e4686d1d24410f
Saleh Sereshki <ssere004@ucr.edu>
2021-11-15 12:25:06 -0800

    some size modification, for memory

--- profile_generator.py
+++ profile_generator.py
@@ -83,7 +83,7 @@ def get_processed_data(cnfg):
     sequences_df = preprocess.convert_assembely_to_onehot(organism_name, sequences, from_file=True)
     return sequences_df, methylations, annot_seq_df_list
 
-def run_experiments(config_list, context_list, window_size, data_size, coverage_threshold=10):
+def run_experiments(config_list, context_list, window_size,block_size, data_size, coverage_threshold=10):
     res = []
     logs = []
     logs.append(['organism_name', 'context', 'data_size', 'window_size', 'slice', 'me_sz', 'ume_sz', 'test_sample_size', 'sample_set', 'profiles', 'x_train', 'x_test', 'x_val', 'dtype'])
@@ -100,15 +100,15 @@ def run_experiments(config_list, context_list, window_size, data_size, coverage_
             test_sample_set = methylated[:test_sample_size]+unmethylated[:test_sample_size]
             methylated = methylated[test_sample_size:]
             unmethylated = unmethylated[test_sample_size:]
-            test_profiles, test_targets = get_profiles(methylations, test_sample_set, sequences_onehot, annot_seqs_onehot, window_size=3200)
+            test_profiles, test_targets = get_profiles(methylations, test_sample_set, sequences_onehot, annot_seqs_onehot, window_size=window_size)
             x_test, y_test = data_preprocess(test_profiles, test_targets, include_annot=True)
             PROFILE_ROWS = x_test.shape[1]
             PROFILE_COLS = x_test.shape[2]
+            x_test_sz = len(x_test)
             np.save('./temporary_files/x_test.npy', x_test)
             np.save('./temporary_files/y_test.npy', y_test)
             del test_profiles, test_targets, x_test, y_test
             data_size = min(data_size, 2*len(methylated), 2*len(unmethylated))
-            block_size = (80, 40)
             model = Sequential()
             model.add(Conv2D(16, kernel_size=(1, PROFILE_COLS), activation='relu', input_shape=(PROFILE_ROWS, PROFILE_COLS, 1)))
             model.add(Reshape((block_size[0], block_size[1], 16), input_shape=(PROFILE_ROWS, 1, 16)))
@@ -125,23 +125,24 @@ def run_experiments(config_list, context_list, window_size, data_size, coverage_
                     break
                 sample_set = methylated[slice:slice+step]+unmethylated[slice:slice+step]
                 random.shuffle(sample_set)
-                profiles, targets = get_profiles(methylations, sample_set, sequences_onehot, annot_seqs_onehot, window_size=3200)
+                profiles, targets = get_profiles(methylations, sample_set, sequences_onehot, annot_seqs_onehot, window_size=window_size)
                 X, Y = data_preprocess(profiles, targets, include_annot=True)
                 x_train, x_val, y_train, y_val = split_data(X, Y, pcnt=0.1)
                 with tf.device('/device:GPU:0'):
                     model.fit(x_train, y_train, batch_size=32, epochs=45, verbose=0, validation_data=(x_val, y_val))
-                x_test = np.load('./temporary_files/x_test.npy')
-                y_test = np.load('./temporary_files/y_test.npy')
-                y_pred = model.predict(x_test)
                 logs.append([organism_name, context, data_size, window_size, slice, me_sz, ume_sz,
-                             test_sample_size, len(sample_set), len(profiles), len(x_train), len(x_test), len(x_val), x_train.dtype])
-                np.savetxt("logs.csv", logs, delimiter=", ", fmt='% s')
-                step_res = [organism_name, context, 'seq-annot', window_size, slice, accuracy_score(y_test, y_pred.round()),
-                            f1_score(y_test, y_pred.round()), precision_score(y_test, y_pred.round()), recall_score(y_test, y_pred.round())]
-                del x_test, y_test
-                print(step_res)
-                res.append(step_res)
-                np.savetxt("GFG.csv", res, delimiter =", ", fmt ='% s')
+                         test_sample_size, len(sample_set), len(profiles), len(x_train), x_test_sz, len(x_val), x_train.dtype])
+            np.savetxt("logs.csv", logs, delimiter=", ", fmt='% s')
+            x_test = np.load('./temporary_files/x_test.npy')
+            y_test = np.load('./temporary_files/y_test.npy')
+            y_pred = model.predict(x_test)
+
+            step_res = [organism_name, context, 'seq-annot', window_size, accuracy_score(y_test, y_pred.round()),
+                        f1_score(y_test, y_pred.round()), precision_score(y_test, y_pred.round()), recall_score(y_test, y_pred.round())]
+            del x_test, y_test
+            print(step_res)
+            res.append(step_res)
+            np.savetxt("GFG.csv", res, delimiter =", ", fmt ='% s')
 
 def data_preprocess(X, Y, include_annot=False):
     if not include_annot:
--- run.py
+++ run.py
@@ -7,4 +7,14 @@ context_list = [
     'CHH']
 window_size = 3200
 data_size = 500000
-pg.run_experiments(config_list, context_list, window_size, data_size, coverage_threshold=10)
+window_sizes = [
+    100,
+    200,
+    400,
+    800,
+    1600,
+    3200
+]
+block_sizes = [(10, 10), (20, 10), (20, 20), (40, 20), (40, 40), (80, 40)]
+for w in range(len(window_sizes)):
+    pg.run_experiments(config_list, context_list, window_sizes[w], block_sizes[w], data_size, coverage_threshold=10)
