ce452951eedc67ce0ef6dd9d5d08fc01fdc5644e
Saleh Sereshki <ssere004@ucr.edu>
2021-11-18 18:03:41 -0800

    lower data sizes

--- configs.py
+++ configs.py
@@ -1,4 +1,5 @@
 
+root = '/home/csgrads/ssere004/Organisms/'
 Arabidopsis_config = {
     'methylation_address': '/home/csgrads/ssere004/Organisms/Arabidopsis/SRR3171614_1_trimmed_bismark_bt2.CX_report.txt',
     'seq_address': '/home/csgrads/ssere004/Organisms/Arabidopsis/GCF_000001735.4_TAIR10.1_genomic.fa',
@@ -14,6 +15,9 @@ Cowpea_config = {
 }
 
 Rice_config = {
+    'methylation_address': root + 'Rice/SRR618545_final_bismark_bt2.CX_report.txt',
+    'seq_address': root + 'Rice/IRGSP-1.0_genome.fasta',
+    'annot_address': root + 'Rice/transcripts_exon.gff',
     'organism_name': 'Rice',
     'annot_types': ['mRNA', 'exon']
 }
