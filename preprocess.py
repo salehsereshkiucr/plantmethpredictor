a1b7ca70874d38042e5950c59f6a133ac7339af1
Saleh Sereshki <ssere004@ucr.edu>
2021-11-19 01:12:53 -0800

    lower data sizes

--- preprocess.py
+++ preprocess.py
@@ -115,7 +115,9 @@ def methylations_sampler(methylated_size, unmethylated_size):
     return shuffled_m, shuffled_um
 
 def seperate_methylations(organism_name, methylations, ratio = 0.1, from_file=True):
-    if from_file:
+    fn_train = './dump_files/'+organism_name+'_methylations_train.csv'
+    fn_test = './dump_files/'+organism_name+'_methylations_test.csv'
+    if from_file and path.exists(fn_test) and path.exists(fn_train):
         methylations_train = pd.read_csv('./dump_files/'+organism_name+'_methylations_train.csv', header=0)
         methylations_test = pd.read_csv('./dump_files/'+organism_name+'_methylations_test.csv', header=0)
         return methylations_train, methylations_test
