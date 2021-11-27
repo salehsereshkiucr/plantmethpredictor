599262a07f898eb36876a97e265713809ced1c33
Saleh Sereshki <ssere004@ucr.edu>
2021-11-26 16:23:24 -0800

    lower data sizes

--- run.py
+++ run.py
@@ -22,4 +22,4 @@ block_sizes = [(10, 10), (20, 10), (20, 20), (40, 20), (40, 40), (80, 40)]
 final_res = []
 #res = pg.run_experiments(config_list, context_list, 3200, (80, 40), data_size, coverage_threshold=10)
 
-res = pg.run_experiments(config_list, context_list, 3200, (80, 40), data_size, [0, 40000, 80000, 120000, 200000, 400000, 600000], coverage_threshold=10, include_annot=False)
+res = pg.run_experiments(config_list, context_list, window_sizes, block_sizes, data_size, [0, 500000], coverage_threshold=10, include_annot=False)
