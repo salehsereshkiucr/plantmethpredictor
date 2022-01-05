b757e3345809e52951557b46044f055818e4d632
Saleh Sereshki <ssere004@ucr.edu>
2022-01-04 23:09:25 -0800

    lower data sizes

--- test_models.py
+++ test_models.py
@@ -18,8 +18,6 @@ for cnfg in cnfgs:
         model = keras.models.load_model('./models/' + organism_name+context + '499983200.mdl')
         for test_config in cnfgs:
             test_organism = test_config['organism_name']
-            if organism_name == test_organism:
-                continue
             sequences_onehot, methylations, annot_seqs_onehot, num_to_chr_dic = pg.get_processed_data(test_config, context, coverage_threshold=10)
             x_test, y_test = pg.test_sampler(methylations, sequences_onehot, annot_seqs_onehot, window_size, num_to_chr_dic, include_annot=include_annot)
             tag = 'seq-only'
@@ -30,5 +28,5 @@ for cnfg in cnfgs:
             step_res = [organism_name, test_organism, context, tag, window_size, 500000, len(x_test), accuracy_score(y_test, y_pred.round()),
                                 f1_score(y_test, y_pred.round()), precision_score(y_test, y_pred.round()), recall_score(y_test, y_pred.round())]
             res.append(step_res)
-            np.savetxt("GFG_cross_organism.csv", res, delimiter =", ", fmt ='% s')
+            np.savetxt("GFG_cross_organism1.csv", res, delimiter =", ", fmt ='% s')
 
