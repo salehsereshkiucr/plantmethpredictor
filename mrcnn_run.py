e82c9dd7b5b88bf45358c4473c640b8a7d7452e7
Saleh Sereshki <ssere004@ucr.edu>
2022-01-13 17:33:49 -0800

    mrcnn added

--- mrcnn_run.py
+++ mrcnn_run.py
@@ -2,6 +2,7 @@ import mrcnn
 import configs as configs
 import numpy as np
 config_list = [configs.Arabidopsis_config, configs.Cowpea_config, configs.Rice_config, configs.Cucumber_config, configs.Tomato_config]
+config_list = [configs.Cowpea_config, configs.Rice_config, configs.Cucumber_config, configs.Tomato_config]
 contexts = ['CG', 'CHG', 'CHH']
 
 cnfg = config_list[0]
