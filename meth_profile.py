d27607352d0872c6f50a2059e658fb7674564abd
Saleh Sereshki <ssere004@ucr.edu>
2022-02-11 07:19:28 -0800

    mrcnn added

--- meth_profile.py
+++ meth_profile.py
@@ -11,17 +11,16 @@ import profile_generator as pg
 import configs as configs
 from os.path import exists
 
-def profiler(cnfg, methylations, context, datasize, window_size=20):
-    organism_name = cnfg['organism_name']
-    half_w = int(window_size/2)
+def input_maker(methylations, datasize, window_size, organism_name, from_file, context, half_w):
     methylations = methylations.sort_values(["chr", "position"], ascending=(True, True))
     chrs_counts = methylations['chr'].value_counts()
     last_chr_pos = {}
     chrnums = list(chrs_counts.index)
     sum = 0
     for i in range(len(chrnums)):
-        last_chr_pos[i] = sum+chrs_counts[i]-1
-        sum += chrs_counts[i]
+        if i in chrs_counts.keys():
+            last_chr_pos[i] = sum+chrs_counts[i]-1
+            sum += chrs_counts[i]
     # last_chr_pos ==> {0: 5524, 1: 1042784, 2: 1713034, 3: 2550983, 4: 3205486, 5: 4145381, 6: 4153872}
     # methylations.iloc[2550983] => chr 3.0 position    23459763.0
     # methylations.iloc[2550984] => chr 4.0 position    1007
@@ -32,7 +31,7 @@ def profiler(cnfg, methylations, context, datasize, window_size=20):
     mlevels = np.asarray(mlevels)
     X = np.zeros((datasize, window_size))
     Y = np.zeros(datasize)
-    if exists('./temporary_files/' +organism_name+'_meth_avlbls.npy'):
+    if from_file and exists('./temporary_files/' +organism_name+'_meth_avlbls.npy'):
         avlbls = np.load('./temporary_files/' +organism_name+'_meth_avlbls.npy')
     else:
         avlbls = np.asarray(idxs)
@@ -40,18 +39,34 @@ def profiler(cnfg, methylations, context, datasize, window_size=20):
             if lcp > 0 and lcp < len(mlevels) - window_size:
                 avlbls = np.setdiff1d(avlbls, range(lcp-half_w, lcp+half_w))
         np.save('./temporary_files/' +organism_name+'_meth_avlbls.npy', avlbls)
-
     smple = random.sample(list(avlbls), datasize)
     count_errored = 0
+    print('border conditions: ', np.count_nonzero(np.asarray(smple) < half_w))
     for index, p in enumerate(smple):
         try:
             X[index] = np.concatenate((mlevels[p-half_w: p], mlevels[p+1: p+half_w+1]), axis=0)
             Y[index] = 0 if mlevels[p] < 0.5 else 1
         except ValueError:
+            print(index, p)
             count_errored += 1
     X = X.reshape(list(X.shape) + [1])
     print(count_errored, ' profiles faced error')
     return X, Y
+
+
+def profiler(cnfg, methylations, context, datasize, window_size=20, from_file=False, threshold=0.5):
+    #methylations = methylations[(methylations['mlevel'] > 0.8) | (methylations['mlevel'] < 0.2)]
+    organism_name = cnfg['organism_name']
+    half_w = int(window_size/2)
+    methylated = methylations[methylations['mlevel'] > threshold]
+    unmethylated = methylations[methylations['mlevel'] <= threshold]
+
+    X_methylated, Y_methylated = input_maker(methylated, int(datasize/2), window_size, organism_name, from_file, context, half_w)
+    X_unmethylated, Y_unmethylated = input_maker(unmethylated, int(datasize/2), window_size, organism_name, from_file, context, half_w)
+
+    return np.concatenate((X_methylated, X_unmethylated), axis=0), np.concatenate((Y_methylated, Y_unmethylated), axis=0)
+
+
 
 def run_experiment(X, Y, window_size=20, test_percent=0.2, test_val_percent = 0.5):
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=test_percent, random_state=None)
