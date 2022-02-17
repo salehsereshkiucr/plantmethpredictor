f1f97529dbe4ba33727b5c1d4fd8c32adf3a56f8
Saleh Sereshki <ssere004@ucr.edu>
2022-02-17 12:41:34 -0800

    mrcnn added

--- cpgenie_run.py
+++ cpgenie_run.py
@@ -3,8 +3,9 @@ import cpgenie_mine as cpgm
 context_list = [
     'CHG',
     'CHH']
+
 config_list = [configs.Arabidopsis_config, configs.Cowpea_config, configs.Rice_config, configs.Cucumber_config, configs.Tomato_config]
-config_list = [configs.Cowpea_config]
+config_list = [configs.Rice]
 
 
-cpgm.run_experiments(config_list, context_list, [0, 500000], coverage_threshold=10, include_annot=False, memory_chunk_size=10000)
+cpgm.run_experiments(config_list, ['CHH'], [0, 500000], coverage_threshold=10, include_annot=False, memory_chunk_size=10000)
